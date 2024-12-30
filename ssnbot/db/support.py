e859c461099d6c53b8a8c5bc707e37283c9e4f12
SilkSmithavibz <Silksmithavibz@gmail.com>
2024-12-31 00:14:52 +0530

    Update support.py

--- ssnbot/db/support.py
+++ ssnbot/db/support.py
@@ -1,28 +1,27 @@
 import asyncio
-from pyrogram.errors import FloodWait
-from pyrogram import enums
+from aiogram import Bot, types
+from aiogram.types import ChatAction
 from ssnbot.db.sql import query_msg, del_user
 from ssnbot import LOGGER
 
-
-async def users_info(bot):
+async def users_info(bot: Bot):
     users = 0
     blocked = 0
     identity = await query_msg()
     for user in identity:
         user_id = int(user[0])
-        name = bool()
+        name = None
         try:
-            name = await bot.send_chat_action(user_id, enums.ChatAction.TYPING)
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
+            # Send typing action to the user (replaces 'send_chat_action' in aiogram)
+            name = await bot.send_chat_action(user_id, ChatAction.TYPING)
+        except asyncio.exceptions.TimeoutError:  # Handling timeout
+            await asyncio.sleep(1)
         except Exception:
             pass
-        if bool(name):
+        if name:
             users += 1
         else:
             await del_user(user_id)
             LOGGER.info("Deleted user id %s from broadcast list", user_id)
             blocked += 1
     return users, blocked
-
